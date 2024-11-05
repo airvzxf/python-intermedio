c3ac0686c88dbdbacc21690af498ffcb47b65143
Israel Roldan <israel.alberto.rv@gmail.com>
2024-11-04 23:35:28 -0600

    Added the second section of the workshop.
    Signed-off-by: Israel Roldan <israel.alberto.rv@gmail.com>

---
+++ src/codigo/python-intermedio/02-debugging.py
@@ -0,0 +1,15 @@
+import pdb
+
+
+def do_something(name):
+    # STEP 3: Add a breakpoint in line 1 of the function do_something.
+    pdb.set_trace()
+    # breakpoint()
+    return f"I don't want to do anything, {name}"
+
+
+if __name__ == '__main__':
+    # STEP 1: Execute the file and explain the below sentences.
+    my_name = "Genaro"
+    # STEP 2: Add a breakpoint in line 1 of the function do_something.
+    print(do_something(my_name))
